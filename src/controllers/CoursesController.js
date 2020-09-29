1666c2accf22350876375a23541796ff86a2a66c
Publio Blenilio <publio.blenilio@gmail.com>
2020-09-29 15:37:09 -0300

    update courses controller

--- src/controllers/CoursesController.js
+++ src/controllers/CoursesController.js
@@ -4,14 +4,19 @@ const Uuid = require('../utils/uuid');
 
 const tableName = 'courses-table';
 
-const index = async () => {
+const index = async ({ queryParams }) => {
+  const { byProfessorId } = queryParams;
   const courses = await Dynamo.scan(tableName).catch((err) => {
     console.log('error in Dynamo Scan', err);
 
     return [];
   });
 
-  return API_Responses._200(courses);
+  return API_Responses._200(
+    byProfessorId ?
+      courses.filter(el => el.teacher.id === byProfessorId) :
+      courses
+  );
 };
 
 const show = async ({ pathParameters }) => {
@@ -31,17 +36,17 @@ const show = async ({ pathParameters }) => {
 };
 
 const store = async ({ body }) => {
-  const { name, description, teacher } = JSON.parse(body);
+  const { name, description, teacher, students } = JSON.parse(body);
   const id = Uuid.generate();
 
-  const newCourse = await Dynamo.write({ id, name, description, teacher }, tableName);
+  const newCourse = await Dynamo.write({ id, name, description, teacher, students, classes: [] }, tableName);
 
   return API_Responses._201(newCourse);
 };
 
 const update = async ({ body, pathParameters }) => {
   const { id } = pathParameters;
-  const { name, description, teacher } = JSON.parse(body);
+  const { name, description, teacher, classes, students } = JSON.parse(body);
 
   const course = await Dynamo.get({ id }, tableName).catch((err) => {
     console.log('error in Dynamo Get', err);
@@ -53,13 +58,13 @@ const update = async ({ body, pathParameters }) => {
     return API_Responses._404({ message: 'Course not found' });
   }
 
-  const updatedCourse = await Dynamo.write({ id, name, description, teacher }, tableName);
+  const updatedCourse = await Dynamo.write({ id, name, description, teacher, students, classes }, tableName);
 
   return API_Responses._200(updatedCourse);
 };
 
 const destroy = async ({ pathParameters }) => {
-  const { id  } = pathParameters;
+  const { id } = pathParameters;
 
   const user = await Dynamo.get({ id }, tableName).catch((err) => {
     console.log('error in Dynamo Get', err);
